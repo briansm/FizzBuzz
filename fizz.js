987c41b1174fab588f4a89ade8a9c9e11d620caf
Brian Morales <briansmor09@gmail.com>
2018-02-04 20:22:41 -0600

    Agregando funcion divisible

--- fizz.js
+++ fizz.js
@@ -3,3 +3,12 @@ var numeros=100;
 for(var i=1;i<=numeros;i++){
   document.write(i+"<br/>");
 }
+
+//Funcion que comprueba si es divisible
+function esdiv(iter,num){
+  if(iter%num==0){
+    return true;
+  }else{
+    return false;
+  }
+}
